c8ae16e361a26aadc19ba858828262bd2de9e0e0
James Green <jrgreen@gmail.com>
2018-01-23 23:15:46 +0000

    Connect to DB

--- core/src/main/java/uk/co/greenjam/jrgfinance/core/services/DBInitialiseService.java
+++ core/src/main/java/uk/co/greenjam/jrgfinance/core/services/DBInitialiseService.java
@@ -1,111 +1,113 @@
 package uk.co.greenjam.jrgfinance.core.services;
 
-import com.google.common.base.Charsets;
-
-
-import com.google.common.io.CharStreams;
+import com.day.commons.datasource.poolservice.DataSourcePool;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.LoggerFactory;
 
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import javax.sql.DataSource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.util.Map;
 
 
-@Component( immediate = true,
-            service=DBInitialiseService.class )
+@Component(immediate = true,
+        service = DBInitialiseService.class)
 public class DBInitialiseService {
+    @Reference
+    private DataSourcePool source;
 
-        private static final String DB_INIT_FILE = "initMysqlDB.sql";
+    private static final String DB_INIT_FILE = "initMysqlDB.sql";
+    private static final String DB_DATASOURCE = "jrgfinance.survey";
 
-        private static final String DB_USERNAME = "username";
-        private static final String DB_PASSWORD = "password";
-        private static final String DB_SERVER = "server";
-        private static final String DB_PORT = "port";
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
 
-        private final org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
+    private ComponentContext context = null;
 
-        private ComponentContext context = null;
+    @Activate
+    protected void activate(ComponentContext context) {
+        logger.info("Activating " + this.getClass());
+        this.context = context;
 
-        @Activate
-        protected void activate(ComponentContext context) {
-            logger.info("Activating " + this.getClass());
-                this.context = context;
-
-                String SQLCommands = getResource(DB_INIT_FILE);
-
-                Map dbConfig = getDBConfig();
-                Connection connection = connectToDB(dbConfig);
-                if(connection != null){
-                        logger.info("Connection created...");
-                        if(!tablesExist(connection)){
-                                createTables(connection);
-                        }
-                }
-                else {
-                        logger.error("Unable to connect to database using connection information: " + dbConfig);
-                }
+        Connection connection = getConnection();
+        if (connection != null) {
+            logger.info("Connection created...");
+            if (!tablesExist(connection)) {
+                createTables(connection);
+            }
         }
+    }
 
-        private void createTables(Connection connection) {
-                String SQLCommands = getResource(DB_INIT_FILE);
+    private void createTables(Connection connection) {
+        String SQLCommands = getResource(DB_INIT_FILE);
+    }
+
+    private String getResource(String dbInitFile) {
+        String resource = null;
+        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(dbInitFile);
+        resource = getStringFromInputStream(resourceAsStream);
+        return resource;
+    }
+
+    private boolean tablesExist(Connection connection) {
+        // Default to tables existing, so we don't overwrite tables if this method fails
+        boolean exists = true;
+        return exists;
+    }
+
+    private Map getDBConfig() {
+        Map config = null;
+        return config;
+    }
+
+    private Connection getConnection() {
+        DataSource dataSource = null;
+        Connection con = null;
+
+        try {
+            dataSource = (DataSource) source.getDataSource(DB_DATASOURCE);
+            return dataSource.getConnection();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("Unable to connect to datasource " + DB_DATASOURCE);
         }
+        return null;
+    }
 
-        private String getResource(String dbInitFile) {
-                String resource = null;
-                InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(dbInitFile);
-                resource = getStringFromInputStream(resourceAsStream);
-                return resource;
-        }
+    // convert InputStream to String
+    private static String getStringFromInputStream(InputStream is) {
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
 
-        private boolean tablesExist(Connection connection) {
-                // Default to tables existing, so we don't overwrite tables if this method fails
-                boolean exists = true;
-                return exists;
-        }
+        String line;
+        try {
 
-        private Map getDBConfig() {
-                Map config = null;
-                return config;
-        }
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
 
-        private Connection connectToDB(Map dbConfig) {
-                Connection connection = null;
-                return connection;
-        }
-
-        // convert InputStream to String
-        private static String getStringFromInputStream(InputStream is) {
-                BufferedReader br = null;
-                StringBuilder sb = new StringBuilder();
-
-                String line;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
                 try {
-
-                        br = new BufferedReader(new InputStreamReader(is));
-                        while ((line = br.readLine()) != null) {
-                                sb.append(line);
-                        }
-
+                    br.close();
                 } catch (IOException e) {
-                        e.printStackTrace();
-                } finally {
-                        if (br != null) {
-                                try {
-                                        br.close();
-                                } catch (IOException e) {
-                                        e.printStackTrace();
-                                }
-                        }
+                    e.printStackTrace();
                 }
-
-                return sb.toString();
-
+            }
         }
+
+        return sb.toString();
+
+    }
 
 
 }
