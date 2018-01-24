1270aec634d67db092c85fdce0ad31ed75a9edc7
James Green <jrgreen@gmail.com>
2018-01-24 01:19:15 +0000

    WIP create tables - Need to handle characters in current SQL file or amend SQL file

--- core/src/main/java/uk/co/greenjam/jrgfinance/core/services/DBInitialiseService.java
+++ core/src/main/java/uk/co/greenjam/jrgfinance/core/services/DBInitialiseService.java
@@ -1,6 +1,7 @@
 package uk.co.greenjam.jrgfinance.core.services;
 
 import com.day.commons.datasource.poolservice.DataSourcePool;
+import org.apache.commons.lang3.StringUtils;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -13,6 +14,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -29,7 +32,7 @@ public class DBInitialiseService {
     private static final String DB_SURVEY_TABLE = "survey";
 
 
-    private static final String[] DB_TABLE = new String[] {"TABLE"};
+    private static final String[] DB_TABLE = new String[]{"TABLE"};
 
     private static final String DB_INIT_FILE = "initMysqlDB.sql";
     private static final String DB_DATASOURCE = "jrgfinance.survey";
@@ -47,23 +50,27 @@ public class DBInitialiseService {
         if (connection != null) {
             logger.info("Connection created...");
             if (!noTablesExist(connection)) {
+                logger.info("Creating tables");
                 createTables(connection);
-            }
-            else{
+            } else {
                 logger.info("NOT creating tables.");
             }
         }
     }
 
     private void createTables(Connection connection) {
-        String SQLCommands = getResource(DB_INIT_FILE);
+        InputStream is = getResource(DB_INIT_FILE);
+
+        if (is != null) {
+            executeSQL(is, connection);
+        } else {
+            logger.error("Failed to read SQL Commands");
+        }
+
     }
 
-    private String getResource(String dbInitFile) {
-        String resource = null;
-        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(dbInitFile);
-        resource = getStringFromInputStream(resourceAsStream);
-        return resource;
+    private InputStream getResource(String dbInitFile) {
+        return getClass().getClassLoader().getResourceAsStream(dbInitFile);
     }
 
     private boolean noTablesExist(Connection connection) {
@@ -71,28 +78,28 @@ public class DBInitialiseService {
 
         try {
             DatabaseMetaData metadata = connection.getMetaData();
-            ResultSet rs = metadata.getTables(null, null,DB_COMMENT_TABLE, DB_TABLE);
-            if (rs.next()){
+            ResultSet rs = metadata.getTables(null, null, DB_COMMENT_TABLE, DB_TABLE);
+            if (rs.next()) {
                 logger.info("Comment table found");
                 return true;
             }
-            rs = metadata.getTables(null, null,DB_ADDITIONAL_META_TABLE, DB_TABLE);
-            if (rs.next()){
+            rs = metadata.getTables(null, null, DB_ADDITIONAL_META_TABLE, DB_TABLE);
+            if (rs.next()) {
                 logger.info("Additional meta table found");
                 return true;
             }
-            rs = metadata.getTables(null, null,DB_DATA_TABLE, DB_TABLE);
-            if (rs.next()){
+            rs = metadata.getTables(null, null, DB_DATA_TABLE, DB_TABLE);
+            if (rs.next()) {
                 logger.info("Data table found");
                 return true;
             }
-            rs = metadata.getTables(null, null,DB_METADATA_TABLE, DB_TABLE);
-            if (rs.next()){
+            rs = metadata.getTables(null, null, DB_METADATA_TABLE, DB_TABLE);
+            if (rs.next()) {
                 logger.info("Metadata table found");
                 return true;
             }
-            rs = metadata.getTables(null, null,DB_SURVEY_TABLE, DB_TABLE);
-            if (rs.next()){
+            rs = metadata.getTables(null, null, DB_SURVEY_TABLE, DB_TABLE);
+            if (rs.next()) {
                 logger.info("Survey table found");
                 return true;
             }
@@ -126,33 +133,74 @@ public class DBInitialiseService {
         return null;
     }
 
-    // convert InputStream to String
-    private static String getStringFromInputStream(InputStream is) {
+    private boolean executeSQL(InputStream is, Connection connection) {
+        String delimiter = ";";
         BufferedReader br = null;
         StringBuilder sb = new StringBuilder();
-
+        StringBuilder currentStatement = new StringBuilder();
+        List<String> statements = new ArrayList<String>();
         String line;
         try {
 
             br = new BufferedReader(new InputStreamReader(is));
             while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                line = line.trim();
+                if (line.length() == 0 || line.startsWith("--"))
+                    continue;
+                if (line.endsWith(delimiter)) {
+                    line = line.substring(0, line.length() - delimiter.length()).trim();
+                    if (line.length() > 0) {
+                        if (currentStatement.length() > 0)
+                            currentStatement.append("\n");
+                        currentStatement.append(line);
+                        statements.add(currentStatement.toString());
+                        currentStatement = new StringBuilder();
+                    }
+                } else if (line.toUpperCase().startsWith("DELIMITER")) {
+                    delimiter = line.substring("DELIMITER".length()).trim();
+                } else {
+                    if (currentStatement.length() > 0)
+                        currentStatement.append("\n");
+                    currentStatement.append(line);
                 }
             }
+            br.close();
+            for (String statement : statements) {
+                logger.info("***************************************************************");
+                logger.info(statement);
+                logger.info("***************************************************************");
+                if (execute(connection, statement) < 0)
+                    return false;
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("Failed to execute script - " + e.getMessage());
+            return false;
+        }
+    }
+
+
+    public int execute(Connection connection, String statement) {
+        if (StringUtils.isEmpty(statement)) {
+            return -1;
         }
 
-        return sb.toString();
+        Statement pst = null;
+        try {
 
+            logger.info("***************************************************************");
+            logger.info(statement);
+            logger.info("***************************************************************");
+
+            pst = connection.createStatement();
+            int result = pst.executeUpdate(statement);
+            return result >= 0 ? result : 0;
+        } catch (SQLException e) {
+            logger.error("Couldn't execute statement - " + statement);
+            logger.error(e.getMessage());
+        }
+        return -1;
     }
 
 
